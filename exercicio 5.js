7635214db80b73718fb5a5f5ffb9774932208d45
lka_dev <lucagabrielsantos5@gmail.com>
2024-12-17 09:48:01 -0300

    Add files via upload

---
+++ exercicio 5.js
@@ -0,0 +1,13 @@
+//new object -> Object.prototype        
+ const objA = {
+    chaveA: 'A'
+ };
+ const objB = {
+    chaveB: 'B'
+ };
+ const objC = new Object();
+ objC.chaveC = 'C'
+ Object.setPrototypeOf(objB, objA);
+ Object.setPrototypeOf(objC, objB);
+ 
+ console.log(objB.chaveA);
