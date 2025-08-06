fa92432916a8df9dd31ec4b1ecca4737273c0058
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 08:41:01 -0300

    Update desafio#003.py

--- desafio#003.py
+++ desafio#003.py
@@ -1,4 +1,4 @@
 n1 = int(input('digite um numero: '))
 n2 = int(input('digite outro numero: '))
 soma = n1 + n2
-print('a soma entre {} e {}, será {}'.format(n1, n2, soma))
+print(f'a soma entre {n1} e {n2}, será {soma}')
