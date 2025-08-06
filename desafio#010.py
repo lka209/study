ccb466fedd1360a7643ef6b769afcc86abac9697
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 08:44:03 -0300

    Update desafio#010.py

--- desafio#010.py
+++ desafio#010.py
@@ -1,3 +1,3 @@
 real= float(input('quanto dinheiro tem na carteira?R$'))
 dolar = real /5.49
-print('com R$ {:.1f}, voce pode comprar US$ {:.2f}'.format( real,dolar))
+print(f'com R$ {real:.1f}, voce pode comprar US$ {dolar:.2f}')
