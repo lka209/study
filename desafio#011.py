ea8fa07b1cc88c0c2b018f1a42f381b80bd59fdd
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 08:45:10 -0300

    Update desafio#011.py

--- desafio#011.py
+++ desafio#011.py
@@ -3,4 +3,5 @@ alt = float(input('qual a altura da parade?'))
 area = larg*alt
 print(f'sua parade tem dimensão de {larg}x{alt} e sua area tem {area}m² ')
 tinta = area/2
-print(f'para pintar essa parade, voce vai precisar {tinta}L')
+print(f'para pintar essa parade, voce vai precisar {tinta}')
+
