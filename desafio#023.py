6278a89fc5bd95d55afaa2b17dd44801e11d5e82
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 09:00:40 -0300

    Update desafio#023.py

--- desafio#023.py
+++ desafio#023.py
@@ -3,8 +3,9 @@ u = numero // 1 % 10
 d = numero // 10 % 10
 c = numero // 100 % 10
 m = numero // 1000 % 10
-print('analisando o numero: {}'.format(numero))
-print('unidade:{}'.format(u))
-print('dezena:{}'.format(d))
-print('centena:{}'.format(c))
-print('milhar: {}'.format(m))
+print(f'analisando o numero: {}'.format(numero))
+print(f'unidade:{u}')
+print(f'dezena:{d}')
+print(f'centena:{c}')
+
+print(f'milhar: {m}')
