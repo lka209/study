0350a43c4827f44288502eb470b6900ac6b045e7
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 08:55:31 -0300

    Update desafio#016.py

--- desafio#016.py
+++ desafio#016.py
@@ -3,4 +3,5 @@
 # print('o valor digitado foi {} é a sua porção inteira é {}'.format(num,trunc(num)))
 
 num = float(input('digite um valor:'))
-print('o valor digitado foi {} e a sua porção inteira é {}'.format(num,int(num)))
+
+print(f'o valor digitado foi {num} e a sua porção inteira é {int}')
