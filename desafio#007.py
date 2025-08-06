850ba23fe0418ebdb24d57df940ea56a3e9b9f6c
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 08:42:42 -0300

    Update desafio#007.py

--- desafio#007.py
+++ desafio#007.py
@@ -2,4 +2,5 @@ n1 = float(input('primeira nota do aluno: '))
 n2= float(input('segunda nota do aluno: '))
 n3= float(input('terceira nota do aluno: '))
 m = n1+n2+n3/2
-print('a media entre {} e {} e {}, é igual a {}'.format(n1,n2,n3,m))
+print(f'a media entre {n1} e {n2} e {n3}, é igual a {m}')
+
