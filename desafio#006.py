c1e90f78355ce845bfd5b6eeef1740bc01127a13
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 08:42:18 -0300

    Update desafio#006.py

--- desafio#006.py
+++ desafio#006.py
@@ -2,4 +2,5 @@ n = int(input('digite um numero: '))
 D = n*2
 T = n*3
 R = n**2
-print('apos analizar o valor{},o dobro  é {},o triplo  é {}, a raiz quadrada é {}'.format(n,D,T,R))
+print(f'apos analizar o valor{n},o dobro  é {D},o triplo  é {T}, a raiz quadrada é {R}')
+
