c4e454399c37932a6013bd2735d4234bef005e7b
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 08:46:23 -0300

    Update desafio#013.py

--- desafio#013.py
+++ desafio#013.py
@@ -1,3 +1,3 @@
 salario = float(input('digite seu salario:R$'))
 novo = salario + (salario * 15/100)
-print('o salario vai ser {}, apos o aumento será {:.1f}'.format(salario, novo))
+print(f'o salario vai ser {salario}, apos o aumento será {novo:.1f}')
