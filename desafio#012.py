e33008f6739573b49e8e2a7fec6ca57fadc9abf2
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 08:45:52 -0300

    Update desafio#012.py

--- desafio#012.py
+++ desafio#012.py
@@ -1,5 +1,6 @@
 preco = float(input('qual o preço do produto? R$ '))
 novo = preco - (preco * 5/100)
-print('o produto custava R${}, na promoção com desconto de 5% vai custar R${}'.format(preco, novo))
+print(f'o produto custava R${preco}, na promoção com desconto de 5% vai custar R${novo}')
+
 
 
