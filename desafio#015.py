e45aac21ee0e89300c9f78b05873bbced0aa1e8b
lka_dev <lucagabrielsantos5@gmail.com>
2025-08-06 08:49:15 -0300

    Update desafio#015.py

--- desafio#015.py
+++ desafio#015.py
@@ -1,4 +1,5 @@
 dias = int(input('quantos dias alugados?'))
 km = float(input('quantos km rodados?'))
 pago = dias * 60 + km * 0.15
-print('o total a pagar é de R${:.2f}'.format(pago))
+print(f'o total a pagar é de R${pago:.2f}')
+
